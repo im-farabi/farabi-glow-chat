d9344b8fd68e68eee1fa440b956a0b31b734b79a
im-farabi <mbraveguy@gmail.com>
2025-11-07 23:02:37 +0600

    Update recommendedQuestions.ts

--- src/data/recommendedQuestions.ts
+++ src/data/recommendedQuestions.ts
@@ -3,10 +3,9 @@
  * Add or modify questions here to customize the dropdown suggestions
  */
 export const recommendedQuestions = [
-  "What is 67 meme means",
-  "How do I make a website?",
-  "Explain quantum computing simply",
-  "What's the difference between HTML and CSS?",
-  "How does AI work?",
-  "Write a basic Python script for me",
+  "Do we control life or is it decided?",
+  "What’s something real that feels fake?,
+  "If God’s exist, why do bad things happen?",
+  "Can our thoughts really change reality?",
+  "What are the dark sides of businesses?",
 ];
